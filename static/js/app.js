f882cc4bb69d8e247697a4c8540ef8612087bf47
tianiachan <tiania.chan@gmail.com>
2020-04-25 06:23:09 -0700

    working gauge

--- static/js/app.js
+++ static/js/app.js
@@ -1,34 +1,39 @@
 var samples = [];
 var sampleMetadata = [];
 //use d3 to read in json
-d3.json("/samples.json", function (data) 
-{
+d3.json("/samples.json", function (data) {
   //sample data
   samples = data.samples;
   //metadata data
   sampleMetadata = data.metadata;
   //populate dropdown with IDs
-  var optionID = []
-  //get data from the selection
+  // var optionID = [];
+  //get dropdown menu on html
   var dropOption = d3.select("#selDataset");
+  // default option
+  dropOption
+    .append("option")
+    .text("select option")
+    .attr("value", "none");
   //populate dropdown menu with all of the IDs using forEach
-  samples.forEach(sample => 
-    {
-      //using dropdown, append the text and value as the id
-      dropOption
-        .append("option").
-        text(sample.id).
-        attr("value", sample.id);
-      // append ids for future use?
-      optionID.push(sample.id);
-    });
+  samples.forEach(sample => {
+    //using dropdown, append the text and value as the id
+    dropOption
+      .append("option")
+      .text(sample.id)
+      .attr("value", sample.id);
+    // append ids for future use?
+    // optionID.push(sample.id);
+  });
 });
 
 // helper to print metadata to the panel on html
-function printMetadata(patientMeta) 
-{
+function printMetadata(patientMeta) {
+  
   //access the panel body
   var pulledMetaData = d3.select("#sample-metadata");
+  //clear old data before populating with new data
+  pulledMetaData.text(" ");
   //populate panel body - turn into a helper function?
   pulledMetaData
     .append("p")//append p to make new line every time - better solution?
@@ -48,8 +53,7 @@ function printMetadata(patientMeta)
 }
 
 //helper for bubble chart population
-function makeBubble(otuIDs, otuLabels, sampleValues) 
-{
+function makeBubble(otuIDs, otuLabels, sampleValues) {
   //trace for bubble chart
   var traceBubble =
   {
@@ -80,8 +84,7 @@ function makeBubble(otuIDs, otuLabels, sampleValues)
 }
 
 //helper to make bar chart
-function makeBar(otuIDstring, sampleValues) 
-{
+function makeBar(otuIDstring, sampleValues) {
   //make trace  
   var traceBar = {
     //set ID to string  to read as labels
@@ -102,14 +105,29 @@ function makeBar(otuIDstring, sampleValues)
     title: "Bar Chart"
   };
 
-//   //go to the html item with id called plot so know where to place it
+  //   //go to the html item with id called plot so know where to place it
   Plotly.newPlot("bar", dataBar, layoutBar);
 }
 
 //helper to make gauge chart
-function makeGauge()
-{
+function makeGauge(washingF) {
+  var data =
+    [{
+      domain: { x: [0, 1], y: [0, 1] },
+      value: washingF,
+      title: { text: "Belly Button Washing Frequency -  Scrubs per Week" },
+      type: "indicator",
+      mode: "gauge+number",
+      gauge: { axis: {range: [0, 10]}}
+    }];
 
+  var layout = 
+    { 
+      width: 600, 
+      height: 500, 
+      margin: { t: 0, b: 0 } 
+    };
+  Plotly.newPlot('gauge', data, layout);
 }
 
 
@@ -117,42 +135,38 @@ function makeGauge()
 d3.select("#selDataset").on("change", optionChanged(this.value));
 
 // //take value from when option is changed to populate maps using helper functions
-function optionChanged(sampleID)
-{
-  samples.forEach(patientSample =>
-    {
-      // if the id in the array matches the Id that was brought in, call bar and bubble
-      if(sampleID === patientSample.id)
-      {
-        //declare empty array to store the ids and string version of ID
-        var otuIDs = [];
-        var otuIDstring = [];
-        //change all OTU ids to strings to use in bargraph
-        for (i = 0; i < patientSample.otu_ids.length; i++) 
-        {
-          //put OTU in front of the ID and put to string
-          var tempID = patientSample.otu_ids[i].toString();
-          //push to array
-          otuIDs.push(tempID);
-          otuIDstring.push("OTU " + tempID);
-        }
-      
-        //labels
-        var otuLabels = patientSample.otu_labels;
-      
-        //values
-        var sampleValues = patientSample.sample_values;
-        makeBar(otuIDstring, sampleValues);
-        makeBubble(otuIDs, otuLabels, sampleValues);
+function optionChanged(sampleID) {
+  samples.forEach(patientSample => {
+    // if the id in the array matches the Id that was brought in, call bar and bubble
+    if (sampleID === patientSample.id) {
+      //declare empty array to store the ids and string version of ID
+      var otuIDs = [];
+      var otuIDstring = [];
+      //change all OTU ids to strings to use in bargraph
+      for (i = 0; i < patientSample.otu_ids.length; i++) {
+        //put OTU in front of the ID and put to string
+        var tempID = patientSample.otu_ids[i].toString();
+        //push to array
+        otuIDs.push(tempID);
+        otuIDstring.push("OTU " + tempID);
       }
-    })
-    //populate meta data using forEach
-  sampleMetadata.forEach(patientMetaData =>
-    {
-      // if id matches, call the metadatafunction
-      if (sampleID === patientMetaData.id.toString())
-      {
-        printMetadata(patientMetaData);
-      }
-    })
+
+      //labels
+      var otuLabels = patientSample.otu_labels;
+
+      //values
+      var sampleValues = patientSample.sample_values;
+      makeBar(otuIDstring, sampleValues);
+      makeBubble(otuIDs, otuLabels, sampleValues);
+    }
+  })
+  //populate meta data using forEach
+  sampleMetadata.forEach(patientMetaData => {
+    // if id matches, call the metadatafunction
+    if (sampleID === patientMetaData.id.toString()) {
+      printMetadata(patientMetaData);
+      // console.log(patientMetaData.wfreq);
+      makeGauge(patientMetaData.wfreq);
+    }
+  })
 }
